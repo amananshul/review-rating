9c4793d2f46caeeef45addf3f5bc0b0bcbe079bb
prajwalsinghtiwari <102579636+prajwalsinghtiwari@users.noreply.github.com>
2024-03-25 01:20:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/api', companyRoutes); // Assuming companies API endpoints are under '/
 
 // MongoDB connection
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {
   useNewUrlParser: true,
